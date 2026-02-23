a27ad2f444c2af89b7475c516eee2a6071378ac2
onuchakvlad-beep <onuchak.vlad@gmail.com>
2026-02-23 21:42:28 +0200

    main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     #"fsdfsdfdsfdf"
-
+print("it s worked")
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
